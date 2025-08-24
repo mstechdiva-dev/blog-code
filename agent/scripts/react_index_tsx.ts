61f73e511e2b18dca3337913b9c09b4141bad2d5
mstechdiva-dev <cmelearn@mstechdiva.com>
2025-08-24 01:42:53 -0700

    Create react_index_tsx.ts
    new file

---
+++ agent/scripts/react_index_tsx.ts
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import ClaudeAIAgent from './App';
+import { StrictMode } from 'react';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <StrictMode>
+    <ClaudeAIAgent />
+  </StrictMode>
+);
